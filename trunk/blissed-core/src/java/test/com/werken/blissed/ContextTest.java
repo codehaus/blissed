7c7f610da0fa4f131793a58dfb274428ca214ba6
werken <werken@1f5963fc-b272-0410-b324-d70d6f45b145>
2002-07-06 14:58:31 +0000

    Couple of more transition tests.

--- trunk/blissed-core/src/java/test/com/werken/blissed/ContextTest.java
+++ trunk/blissed-core/src/java/test/com/werken/blissed/ContextTest.java
@@ -6,6 +6,7 @@ public class ContextTest extends TestCase
 {
     private Process process;
     private Context context;
+    private State   state1;
 
     public ContextTest(String name)
     {
@@ -16,6 +17,15 @@ public class ContextTest extends TestCase
     {
         this.process = new Process( "test.process",
                                     "test process" );
+
+        this.state1 = this.process.addState( "state.1",
+                                             "state one" );
+
+        this.process.getStart().setDestination( this.state1 );
+
+        this.state1.addTransition( this.process.getFinish(),
+                                   new BooleanGuard( false ),
+                                   "1 to finish" );
 
         this.context = new Context( this.process );
     }
@@ -57,5 +67,30 @@ public class ContextTest extends TestCase
         this.context.clearVariable( "two" );
 
         assertNull( this.context.getVariable( "two" ) );
+    }
+
+    public void testSpawn()
+    {
+        try
+        {
+            Context spawned = this.context.spawn( this.process );
+            
+            assertSame( this.process,
+                        spawned.getCurrentProcess() );
+            
+            assertSame( this.state1,
+                        spawned.getCurrentNode() );
+
+            assertSame( this.context,
+                        spawned.getParent() );
+        }
+        catch (InvalidMotionException e)
+        {
+            fail( e.getLocalizedMessage() );
+        }
+        catch (ActivityException e)
+        {
+            fail( e.getLocalizedMessage() );
+        }
     }
 }
