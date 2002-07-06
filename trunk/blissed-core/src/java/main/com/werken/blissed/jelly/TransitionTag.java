bdeaea1b613d38cf20eee77983feccd7f8abb960
werken <werken@1f5963fc-b272-0410-b324-d70d6f45b145>
2002-07-06 14:51:21 +0000

    * More jelly tag shells. * Another test-case

--- trunk/blissed-core/src/java/main/com/werken/blissed/jelly/TransitionTag.java
+++ trunk/blissed-core/src/java/main/com/werken/blissed/jelly/TransitionTag.java
@@ -2,7 +2,7 @@ package com.werken.blissed.jelly;
 
 import org.apache.commons.jelly.XMLOutput;
 
-public class ProcessTag extends BlissedTag
+public class TransitionTag extends BlissedTag
 {
     public void doTag(XMLOutput output) throws Exception
     {
--- trunk/blissed-core/src/java/test/com/werken/blissed/ContextTest.java
+++ trunk/blissed-core/src/java/test/com/werken/blissed/ContextTest.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 
 public class ContextTest extends TestCase
 {
+    private Process process;
+    private Context context;
+
     public ContextTest(String name)
     {
         super( name );
@@ -11,13 +14,48 @@ public class ContextTest extends TestCase
 
     public void setUp()
     {
+        this.process = new Process( "test.process",
+                                    "test process" );
+
+        this.context = new Context( this.process );
     }
 
     public void tearDown()
     {
+        this.context = null;
+        this.process = null;
     }
 
-    public void testNothing()
+    public void testVariableManagement()
     {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+
+        assertNull( this.context.getVariable( "one" ) );
+
+        assertNull( this.context.getVariable( "two" ) );
+
+        this.context.setVariable( "one",
+                                  obj1 );
+
+        this.context.setVariable( "two",
+                                  obj2 );
+
+        assertSame( obj1,
+                    this.context.getVariable( "one" ) );
+
+        assertSame( obj2,
+                    this.context.getVariable( "two" ) );
+
+        this.context.clearVariable( "one" );
+
+        assertNull( this.context.getVariable( "one" ) );
+
+        assertSame( obj2,
+                    this.context.getVariable( "two" ) );
+
+        this.context.clearVariable( "two" );
+
+        assertNull( this.context.getVariable( "two" ) );
     }
 }
