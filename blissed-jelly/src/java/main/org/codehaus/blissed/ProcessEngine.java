2bd9f1d06446a128a6a667759e7a552982a8792a
proyal <proyal@1f5963fc-b272-0410-b324-d70d6f45b145>
2003-06-05 19:54:32 +0000

    Allow process data to be passed in when spawning a process. Very useful if starting the process requires that data :)

--- blissed-jelly/src/java/main/org/codehaus/blissed/ProcessEngine.java
+++ blissed-jelly/src/java/main/org/codehaus/blissed/ProcessEngine.java
@@ -1,7 +1,7 @@
 package org.codehaus.blissed;
 
 /*
- $Id: ProcessEngine.java,v 1.2 2003-06-04 21:28:46 proyal Exp $
+ $Id: ProcessEngine.java,v 1.3 2003-06-05 19:54:32 proyal Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
 
@@ -57,7 +57,7 @@ import org.apache.commons.logging.LogFactory;
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  *
- *  @version $Id: ProcessEngine.java,v 1.2 2003-06-04 21:28:46 proyal Exp $
+ *  @version $Id: ProcessEngine.java,v 1.3 2003-06-05 19:54:32 proyal Exp $
  */
 public class ProcessEngine implements Runnable
 {
@@ -197,7 +197,7 @@ public class ProcessEngine implements Runnable
             }
             catch( Exception e )
             {
-                log.error( "Exception serviceing entry", e );
+                log.error( "Exception servicing entry", e );
             }
         }
     }
@@ -303,23 +303,7 @@ public class ProcessEngine implements Runnable
     /** Spawn an instance of a <code>Process</code>.
      *
      *  @param process The process to spawn.
-     *
-     *  @return The <code>ProcessContext</code> representing the
-     *          instance of the newly spawned process.
-     *
-     *  @throws ActivityException If an activity causes an error.
-     *  @throws InvalidMotionException If a motion error occurs while
-     *          attempting to spawn the process.
-     */
-    public ProcessContext spawn( Process process ) throws ActivityException, InvalidMotionException
-    {
-        return spawn( process,
-                      false );
-    }
-
-    /** Spawn an instance of a <code>Process</code>.
-     *
-     *  @param process The process to spawn.
+     *  @param processData The data for the new process
      *  @param async Flag indicating if processing of context should
      *               occur asynchronously, or if it should use the
      *               calling thread for motion.
@@ -332,10 +316,13 @@ public class ProcessEngine implements Runnable
      *          attempting to spawn the process.
      */
     public ProcessContext spawn( Process process,
+                                 Object processData,
                                  boolean async )
         throws ActivityException, InvalidMotionException
     {
         ProcessContext processContext = new ProcessContext( this );
+
+        processContext.setProcessData( processData );
 
         if( async )
         {
@@ -358,6 +345,7 @@ public class ProcessEngine implements Runnable
      *
      *  @param process The process to spawn.
      *  @param parent The parent context.
+     *  @param processData The data to associate with the child process
      *
      *  @return The <code>ProcessContext</code> representing the
      *          instance of the newly spawned process.
@@ -367,12 +355,19 @@ public class ProcessEngine implements Runnable
      *          attempting to spawn the process.
      */
     public ProcessContext spawn( Process process,
-                                 ProcessContext parent )
+                                 ProcessContext parent,
+                                 Object processData )
         throws ActivityException, InvalidMotionException
     {
+        if( null == parent )
+        {
+            throw new NullPointerException( "parent" );
+        }
+
         ProcessContext processContext = new ProcessContext( this, parent );
 
         parent.addChild( processContext );
+        processContext.setProcessData( processData );
 
         addToStartProcessQueue( process,
                                 processContext );
