cd57407e5f0888f2da54c10be1f2662fd27b3d72
proyal <proyal@1f5963fc-b272-0410-b324-d70d6f45b145>
2003-06-11 00:27:22 +0000

    * name individual threads * add some logging statements to aid in debugging processes

--- blissed-jelly/src/java/main/org/codehaus/blissed/ProcessEngine.java
+++ blissed-jelly/src/java/main/org/codehaus/blissed/ProcessEngine.java
@@ -1,7 +1,7 @@
 package org.codehaus.blissed;
 
 /*
- $Id: ProcessEngine.java,v 1.3 2003-06-05 19:54:32 proyal Exp $
+ $Id: ProcessEngine.java,v 1.4 2003-06-11 00:27:22 proyal Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
 
@@ -57,7 +57,7 @@ import org.apache.commons.logging.LogFactory;
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  *
- *  @version $Id: ProcessEngine.java,v 1.3 2003-06-05 19:54:32 proyal Exp $
+ *  @version $Id: ProcessEngine.java,v 1.4 2003-06-11 00:27:22 proyal Exp $
  */
 public class ProcessEngine implements Runnable
 {
@@ -133,7 +133,8 @@ public class ProcessEngine implements Runnable
         for( int i = 0; i < this.numThreads; ++i )
         {
             thread = new Thread( this.threads,
-                                 this );
+                                 this,
+                                 "org.codehaus.blissed.ProcessEngine[" + i + "]" );
 
             thread.start();
         }
@@ -435,6 +436,8 @@ public class ProcessEngine implements Runnable
 
         location.enterState( state );
 
+        if( log.isDebugEnabled() ) log.debug( "Entered state " + state.getName() );
+
         Activity activity = state.getActivity();
 
         if( activity == null )
@@ -442,7 +445,11 @@ public class ProcessEngine implements Runnable
             return;
         }
 
+        if( log.isDebugEnabled() ) log.debug( "Performing activity for " + state.getName() );
+
         activity.perform( context );
+
+        if( log.isDebugEnabled() ) log.debug( "Performed activity for " + state.getName() );
     }
 
     /** Exit a <code>State</code> for a particular
@@ -460,6 +467,8 @@ public class ProcessEngine implements Runnable
         Location location = context.getLocation();
 
         location.exitState( state );
+
+        if( log.isDebugEnabled() ) log.debug( "Exited state " + state.getName() );
     }
 
     /** Finish a <code>Process</code> for a particular
@@ -477,6 +486,8 @@ public class ProcessEngine implements Runnable
         Location location = context.getLocation();
 
         location.finishProcess( process );
+
+        if( log.isDebugEnabled() ) log.debug( "Finished process " + process.getName() );
     }
 
     /** Check a <code>ProcessContext</code> for progress possibilities.
@@ -506,8 +517,18 @@ public class ProcessEngine implements Runnable
               }
 
               List trans = currentState.getTransitions();
+
+              if( 0 == trans.size() )
+              {
+                  log.warn( "No transitions from state '" + currentState.getName()
+                            + "' in process '" + context.getCurrentProcess().getName() + "'" );
+              }
+
               Iterator transIter = trans.iterator();
               Transition eachTrans = null;
+
+              if( log.isDebugEnabled() )
+                  log.debug( "Checking transitions from " + currentState.getName() );
 
               INNER:
                 while( transIter.hasNext() )
@@ -556,6 +577,10 @@ public class ProcessEngine implements Runnable
 
         exitState( origin,
                    context );
+
+        if( log.isDebugEnabled() )
+            log.debug( "Followed transition " + transition.getOrigin().getName()
+                       + " -> " + transition.getDestination().getName() );
 
         Process process = context.getCurrentProcess();
 
